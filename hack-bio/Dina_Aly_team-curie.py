f7125b2f5c0a77d451d623466d5612ad771d7fa7
biologistdodo332 <69098237+biologistdodo332@users.noreply.github.com>
2020-08-02 20:21:30 +0200

    Add files via upload

---
+++ hack-bio/Dina_Aly_team-curie.py
@@ -0,0 +1,4 @@
+print (" my name is Dina Aly Mahmoud Aly ")
+print ("my lanaguge is English")
+print ("my slack username is : @Dina Aly Mahmoud Aly")
+print ("my E_mail is : dinaalymahmoud332@gmail.com")
