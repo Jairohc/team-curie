740f367d3a4070693bc5ed00de567a2b010c5690
jairohc <55994086+Jairohc@users.noreply.github.com>
2020-08-03 12:15:13 -0500

    Update 5_python_2.py

--- test_hb/5_python_2.py
+++ test_hb/5_python_2.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python2.7
-print "Jairo_Hurtado,jairohcx@gmail.com,bash,Jairohcx@biotrust-scientific.slack.com"
+print "Jairo_Hurtado,jairohcx@gmail.com,python,Jairohcx@biotrust-scientific.slack.com"
