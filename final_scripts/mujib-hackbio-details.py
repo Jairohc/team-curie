1d5609ac44cfac86d292dfda663779c45d9cabcf
abisogunmujib <69087096+abisogunmujib@users.noreply.github.com>
2020-08-03 21:51:58 +0100

    Delete mujib-hackbio-details.py

--- final_scripts/mujib-hackbio-details.py
+++
@@ -1,5 +0,0 @@
-Python 3.8.2 (tags/v3.8.2:7b3ab59, Feb 25 2020, 23:03:10) [MSC v.1916 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> print ("my name is : Abisogun mujib olamilekan, my email address is: olamilekanmujib@gmail.com, my biostack is: genomics, my language is: python, : my username is: mujib@biotrust-scientific.slack.com")
-my name is : Abisogun mujib olamilekan, my email address is: olamilekanmujib@gmail.com, my biostack is: genomics, my language is: python, : my username is: mujib@biotrust-scientific.slack.com
->>> 
