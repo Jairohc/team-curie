434011d60a73df2a354b69ebb27bb695e4dc1048
jairohc <55994086+Jairohc@users.noreply.github.com>
2020-08-03 12:15:30 -0500

    Update 6_r-script.r

--- test_hb/6_r-script.r
+++ test_hb/6_r-script.r
@@ -1,2 +1,2 @@
 #!/usr/bin/R
-print("Jairo_Hurtado,jairohcx@gmail.com,bash,Jairohcx@biotrust-scientific.slack.com", quote = FALSE)
+print("Jairo_Hurtado,jairohcx@gmail.com,r-script,Jairohcx@biotrust-scientific.slack.com", quote = FALSE)
