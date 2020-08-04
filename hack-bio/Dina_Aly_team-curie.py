dccae29b4798195b5c484f361cddc67ca50f7630
biologistdodo332 <69098237+biologistdodo332@users.noreply.github.com>
2020-08-04 12:36:17 +0200

    Update Dina_Aly_team-curie.py

--- hack-bio/Dina_Aly_team-curie.py
+++ hack-bio/Dina_Aly_team-curie.py
@@ -1,4 +1,1 @@
-print (" my name is Dina Aly Mahmoud Aly ")
-print ("my lanaguge is English")
-print ("my slack username is : @Dina Aly Mahmoud Aly")
-print ("my E_mail is : dinaalymahmoud332@gmail.com")
+print (" my name is Dina Aly Mahmoud Aly ,my lanaguge is Python,my slack username is : @Dina Aly Mahmoud Aly, genomics,my E_mail is : dinaalymahmoud332@gmail.com")
