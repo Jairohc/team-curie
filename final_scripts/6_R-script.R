14bfa6ad2190d0acc745cdc684fe86549409de08
Jana26 <69088375+Jana26@users.noreply.github.com>
2020-08-04 13:32:09 +0300

    Update 6_R-script.R

--- final_scripts/6_R-script.R
+++ final_scripts/6_R-script.R
@@ -1,1 +1,1 @@
-print("Name: Jana Ismail, E-mail: jana.ismail2607@gmail.com, Language Used: R, Biostack: Drug Development, Username: @Jana", quote = FALSE)
+print("Jana Ismail,jana.ismail2607@gmail.com,R,Drug Development,@Jana", quote = FALSE)
