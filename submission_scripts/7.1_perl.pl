7fd516e5a82bf660cb8e0adeafbec78f21bd6727
tiffanyzee <69090384+tiffanyzee@users.noreply.github.com>
2020-08-06 16:29:37 +0100

    Update 7.1_perl.pl

--- submission_scripts/7.1_perl.pl
+++ submission_scripts/7.1_perl.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/perl
-print 'Ekundayo_Victoria,evisol96@gmail.com,perl,Drug_development,Victoria';
+print 'Ekundayo_Victoria,evisol96@gmail.com,perl,Drug_development,@Victoria';
 print "\n";
